39d2b4cbd161f55c994e1ac54d5075b1284652b2
chaitanya krishna <krishna_c67@yahoo.com>
2018-07-09 10:39:00 +0530

    Add files via upload

---
+++ src/app/shared/users.ts
@@ -0,0 +1,9 @@
+export class Details extends Array<Details> {
+    'name': string;
+    'surname': string;
+    'birthDate': string;
+    'phone': number;
+    'city': string;
+    'street': string;
+    'number': number;
+}
